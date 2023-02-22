d6648a657e8f41892d4349e735a9ecc6623172d3
luccChagas <luccaa.chagas23@gmail.com>
2023-02-22 15:05:45 -0300

    add cli logic

---
+++ service/cli.go
@@ -0,0 +1,24 @@
+package service
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
+
+func CommandLineInterface() {
+	app := &cli.App{
+		Name:  "boom",
+		Usage: "make an explosive entrance",
+		Action: func(*cli.Context) error {
+			fmt.Println("boom! I say!")
+			return nil
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
